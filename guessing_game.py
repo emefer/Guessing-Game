5c85c8981c3408dd58ac3f289277a570af97f104
emefer <54183324+emefer@users.noreply.github.com>
2019-08-21 16:57:51 -0500

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,25 @@
+import random
+
 MAX = 10
 MIN = 1
+
+genNumber = random.randint(MIN, MAX)
+#print ("Generated number: " + str(genNumber))
 print ("Guessing game\n\n")
 number = input ("Insert a number between " + str(MIN) + " and " + str(MAX) + ": ")
-number = int(number)
-while (number < MIN or number > MAX):
-    print ("Number is out of range!\n")
+try:
+    number = int(number)
+except:
+    print ("Please enter only numbers\n")    
+tries = 1
+
+while (number != genNumber):    
+    tries = tries + 1
+    if (genNumber < number):
+        print ("My number is lower. Try again!\n")
+    else:
+        print ("My number is higher. Try again!\n")    
     number = input ("Insert a number between " + str(MIN) + " and " + str(MAX) + ": ")
     number = int(number)
+
+print ("You guessed it in " + str(tries) + " tries!\n")
